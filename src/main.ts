0cedbe72d3da8600d0dc816b2fc8bd6e5367019e
Song-EunJu <jd06280@naver.com>
2022-02-24 14:53:55 +0900

    [DEL] 필요없는 파일 삭제

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(cookieParser('secret'));
 
   const config = new DocumentBuilder().setTitle('DSGM API').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
