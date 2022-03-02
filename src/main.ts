4a77b071219f24006af08398339bb555b581cb2b
ohyujeong <ohlife4@gmail.com>
2022-03-02 16:08:12 +0900

    [FIx] Swagger protect

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
+  app.use(process.env.SWAGGER_PATH, basicAuth({
+    challenge: true,
+    users: {
+      [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+    },
+  }));
+
   const config = new DocumentBuilder().setTitle('DSGM API').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(process.env.API_DOCS, app, document);
+  SwaggerModule.setup(process.env.SWAGGER_PATH, app, document);
 
   await app.listen(5000);
 }
